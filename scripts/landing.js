5bc74c4bb7afd4155b198d4256fe5661bf7df2a4
Samantha Harms <pearl@Samantha-Lynn-Harmss-MacBook-Pro.local>
2016-09-21 09:17:58 -0600

    Assign part 2 best guess

--- scripts/landing.js
+++ scripts/landing.js
@@ -8,10 +8,7 @@ var animatePoints = function(points) {
         points[index].style.msTransform = "scaleX(1) translateY(0)";
         points[index].style.WebkitTransform = "scaleX(1) translateY(0)"; 
     }
-    for (var i = 0; i < points.length; i++){
-        revealPoint(i);
-        
-    }
+    document.pointsArray.forEach(revealPoint);
 };
 
  window.onload = function() {
---
+++ scripts/utilities.js
@@ -0,0 +1,10 @@
+var pointsArray = document.getElementsByClassName('point');
+
+function forEach(array){
+    for (var i = 0; i < array.length; i++){
+        array[i];
+        // callback here, but what function? 
+    }
+}
+
+forEach(pointsArray);
