2ce5ec487264626299da5fe9dd0658135cc27d13
Samantha Harms <pearl@Samantha-Lynn-Harmss-MacBook-Pro.local>
2016-09-23 15:03:21 -0600

    Pushing fixes

--- scripts/album.js
+++ scripts/album.js
@@ -55,13 +55,13 @@ var createSongRow = function(songNumber, songName, songLength) {
      return template;
  };
 
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 var setCurrentAlbum = function(album) {
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -72,12 +72,16 @@ var setCurrentAlbum = function(album) {
      }
  };
  
- window.onload = function() {
-     setCurrentAlbum(albumMarconi);
- };
+window.onload = function() {
+    setCurrentAlbum(albumMarconi);
 
-document.getElementsByClassName('album-cover-art')[0].onclick = function() {
-    var arr = [albumModeselektor, albumMarconi, albumPicasso]
-    for (var i = 0; i < arr.length; i++)
-        setCurrentAlbum(arr[i]);
-}
+var albums = [albumModeselektor, albumMarconi, albumPicasso];
+var index = 1;
+albumImage.addEventListener("click", function(event) {
+    setCurrentAlbum(albums[index]);
+    index++;
+    if (index == albums.length){
+        index = 0;
+        }
+    });
+};
